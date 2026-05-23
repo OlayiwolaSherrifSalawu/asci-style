bb410db2084ee25bd952f83b9a31c8608847c82d
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-05-23 20:42:17 +0100

    added configs

--- api/api.go
+++ api/api.go
@@ -1,2 +1,12 @@
 package api
 
+import "log"
+
+type Application struct {
+	configs
+}
+
+type configs struct {
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+}
--- api/errors.go
+++ api/errors.go
@@ -1,4 +1,11 @@
 package api
 
+type Consterror string
 
-type consterr string
+func (e Consterror) Error() string {
+	return string(e)
+}
+
+const (
+	INTERNAL_SERVER_ERROR = Consterror("An Internal Server Error Occured")
+)
