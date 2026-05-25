5d33bb1dd40dc253978adc22e671fe21d7febfe4
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-05-25 13:58:06 +0100

    added models for the project

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,1 +1,14 @@
 package model
+
+type User struct {
+	UserId       string `json:"userid"`
+	UserName     string `json:"username"`
+	EmailAddress string `json:"emailAddress"`
+	HashPassword string `json:"hashPassword"`
+}
+
+type AsciiStyle struct {
+	UserId    string `json:"userid"`
+	AsciiText string `json:"asciiText"`
+	PlainText string `json:"plainText"`
+}
