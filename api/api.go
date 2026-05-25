6b965a9be50ce6412ae3a6bae69b9acf5434449e
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-05-25 14:20:22 +0100

    added application interface

--- api/api.go
+++ api/api.go
@@ -1,12 +1,1 @@
 package api
-
-import "log"
-
-type Application struct {
-	configs
-}
-
-type configs struct {
-	InfoLogger  *log.Logger
-	ErrorLogger *log.Logger
-}
---
+++ api/config.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+)
+
+type ApplicationInterface interface {
+	Routes()
+	Run(h *http.ServeMux)
+}
+type Application struct {
+	Db *sql.DB
+	configs
+}
+
+type configs struct {
+	Port        string
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+}
+
+func NewApplication(db *sql.DB) *Application {
+	return &Application{
+		Db:      db,
+		configs: configs{Port: ":4000", ErrorLogger: log.Default(), InfoLogger: log.Default()},
+	}
+}
+
+func NewApplicationInterFace(app *Application) ApplicationInterface {
+	return app
+}
