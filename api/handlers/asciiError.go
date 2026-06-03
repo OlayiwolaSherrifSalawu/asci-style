a83c597dce4da675c720506cd0eaf2485bb4622c
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-03 13:33:44 +0100

    added errors in the file

--- api/handlers/asciiError.go
+++ api/handlers/asciiError.go
@@ -1,2 +1,11 @@
 package handlers
 
+import "net/http"
+
+func (e *AsciHandlers) clientError(w http.ResponseWriter, err int) {
+	http.Error(w, http.StatusText(err), err)
+}
+
+func (e *AsciHandlers) serverError(w http.ResponseWriter, err error) {
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
