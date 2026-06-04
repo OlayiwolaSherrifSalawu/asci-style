bb096ddad908fd02842389aa890f90c8025594ba
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-04 14:56:55 +0100

    writing run function

--- api/run.go
+++ api/run.go
@@ -1,3 +1,19 @@
 package api
 
-func (a *Application) Run() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func (a *Application) Run() {
+	// setting up go routine
+	// make your channel
+	quit := make(chan os.Signal, 1)
+	// create a signal to notify you when an issue happen
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	
+	a.configs.InfoLogger = log.New(os.Stdout, "INFO: \t", log.Ldate|log.Ltime)
+	a.configs.ErrorLogger = log.New(os.Stderr, "ERROR: \t", log.Ldate|log.Ltime)
+}
